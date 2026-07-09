bb585848cbaa7f52cde4f84f8ab61c74afcac222
HerikCastro <herik.castrocruz22@gmail.com>
2026-07-09 16:19:06 -0300

    fix: corrigir porta do servidor para deploy no Railway

--- projeto-final/frontend/src/api.js
+++ projeto-final/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:28171' })
+const API = axios.create({ baseURL: 'http://localhost:3000' })
 
 export function setAuthToken(token) {
   if (token) API.defaults.headers.common['Authorization'] = `Bearer ${token}`
