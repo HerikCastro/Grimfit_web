600728284b51e3c51cc0150a284f6c626f8eae54
HerikCastro <herik.castrocruz22@gmail.com>
2026-07-09 16:13:56 -0300

    fix: use environment port for Railway deployment

--- projeto-final/frontend/src/api.js
+++ projeto-final/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:3000' })
+const API = axios.create({ baseURL: 'http://localhost:28171' })
 
 export function setAuthToken(token) {
   if (token) API.defaults.headers.common['Authorization'] = `Bearer ${token}`
