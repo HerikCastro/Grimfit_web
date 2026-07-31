6c66f4d8d9b22cefad4b64a323cf2a8f4db36a8b
HCruzDev <herik.castrocruz22@gmail.com>
2026-07-31 02:37:37 -0300

    Add confirmarSenha middleware to user routes

--- projeto-final/grimfit-backend/src/routes/adminUserRoutes.js
+++ projeto-final/grimfit-backend/src/routes/adminUserRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
+const confirmarSenha = require("../middleware/confirmarSenha");
 
 const {
   getUsers,
@@ -22,6 +23,7 @@ router.put(
   "/:id",
   auth,
   admin,
+  confirmarSenha,
   updateUserType
 );
 
@@ -29,6 +31,7 @@ router.delete(
   "/:id",
   auth,
   admin,
+  confirmarSenha,
   deleteUser
 );
 
